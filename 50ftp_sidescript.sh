08051e67468fd38f0b86abfbfe94b03fe4dc9ca3
Michał Słomkowski <m.slomkowski@gmail.com>
2015-05-13 23:04:45 +0200

    Add FTP removing old backup files.

--- 50ftp_sidescript.sh
+++ 50ftp_sidescript.sh
@@ -22,7 +22,7 @@ $ECHO rm $ARCHIVE.bz2
 
 PAST_DATE=`date --date="$FTP_DELETE_FILES_OLDER_THAN days ago" +%s`
 
-listing=`$ECHO ftp -n $FTP_HOST << EOF2
+listing=`ftp -n $FTP_HOST << EOF2
 user $FTP_LOGIN $FTP_PASSWORD
 binary
 ls
@@ -31,19 +31,22 @@ EOF2
 `
 lista=( $listing )
 
-# loop over our files
+function delete_ftp_file {
+$ECHO ftp -n $FTP_HOST << EOF3
+      user $FTP_LOGIN $FTP_PASSWORD
+      binary
+      delete $1
+      quit
+EOF3
+}
+
 for ((FNO=0; FNO<${#lista[@]}; FNO+=9))
 do
     fileName="${lista[`expr $FNO+8`]}"
     if [[ $fileName =~ (db\-)(${HOSTNAME}\-)(.+)_([0-9]+)(\.tar\.bz2) && ${BASH_REMATCH[4]} -lt $PAST_DATE ]]
     then
-      [[ $VERBOSE -ne 0 ]] && echo "Removing $fileName"
-#      $ECHO ftp -n $FTP_HOST << EOF3
-#      user $FTP_LOGIN $FTP_PASSWORD
-#      binary
-#      delete $fileName
-#      quit
-#      EOF3
+      [[ $VERBOSE -ne 0 ]] && echo "* removing $fileName"
+      $(delete_ftp_file $fileName)
     fi
 done
 
