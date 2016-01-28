7206fc5eee5388a3cf3ff8f5b3106409ada1f808
Michał Słomkowski <m.slomkowski@gmail.com>
2016-01-28 14:13:21 +0100

    Clean PostgreSQL backup script.

--- premount.d/20postgresql.sh
+++ premount.d/20postgresql.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
 
-$ECHO mkdir -p ${PREMOUNT_POSTGRESQL_OUTPUT_DIR}
+${ECHO} mkdir -p ${PREMOUNT_POSTGRESQL_OUTPUT_DIR}
 cd ${PREMOUNT_POSTGRESQL_OUTPUT_DIR}
 
-INVOCATION="$ECHO sudo -u $POSTGRESQL_SUDO_USER"
-DATABASES=$(sudo -u $POSTGRESQL_SUDO_USER psql -tqc 'SELECT datname FROM pg_database where datistemplate = false;')
+INVOCATION="${ECHO} sudo -u ${POSTGRESQL_SUDO_USER}"
+DATABASES=$(sudo -u ${POSTGRESQL_SUDO_USER} psql -tqc 'SELECT datname FROM pg_database where datistemplate = false;')
 
 PG_OPTIONS="--clean "
 
-[[ $VERBOSE -ne 0 ]] &&  PG_OPTIONS+=" --verbose"
+[[ ${VERBOSE} -ne 0 ]] &&  PG_OPTIONS+=" --verbose"
 
 DATE="$(full_date)"
-$ECHO mkdir -p $DATE
-$ECHO chown -R postgres .
+${ECHO} mkdir -p ${DATE}
+${ECHO} chown -R postgres .
 
 # dump globals
-$INVOCATION pg_dumpall $PG_OPTIONS --globals-only -f ${DATE}/${DATE}-globals.sql
+${INVOCATION} pg_dumpall ${PG_OPTIONS} --globals-only -f ${DATE}/${DATE}-globals.sql
 
-for d in $DATABASES
+for d in ${DATABASES}
 do
-    [[ $VERBOSE -ne 0 ]] && echo "* dumping $d database"
+    [[ ${VERBOSE} -ne 0 ]] && echo "* dumping $d database"
     fileName="${DATE}/${DATE}-db-$d.sql"
-    $INVOCATION pg_dump $PG_OPTIONS --create -f $fileName $d
-    $ECHO bzip2 -f9 ${fileName}
-    $ECHO chmod 0400 ${fileName}.bz2
+    ${INVOCATION} pg_dump ${PG_OPTIONS} -f ${fileName} ${d}
+    ${ECHO} bzip2 -f9 ${fileName}
+    ${ECHO} chmod 0400 ${fileName}.bz2
 done
 
 for i in `find . -type d -mtime +${PREMOUNT_POSTGRESQL_DELETE_FILES_OLDER_THAN} | sort`
 do
-    $ECHO rm -r $i
-    [[ $VERBOSE -ne 0 ]] && echo "* removed $i"
+    ${ECHO} rm -r $i
+    [[ ${VERBOSE} -ne 0 ]] && echo "* removed $i"
 done
 
 exit 0
