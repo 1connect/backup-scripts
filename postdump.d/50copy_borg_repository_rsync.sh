b7e851c5df1a3ebff68bdd256ac365ce65952ffa
Michał Słomkowski <m.slomkowski@gmail.com>
2018-11-21 15:11:42 +0100

    Change rsync borg sync to use sftp only.

--- postdump.d/50copy_borg_repository_rsync.sh
+++ postdump.d/50copy_borg_repository_rsync.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-RSYNC_OPTIONS="-rzx --safe-links --delete"
+RSYNC_OPTIONS="--recursive --timeout=60 --compress --safe-links --delete --one-file-system"
 
-[[ ${VERBOSE} -ne 0 ]] && RSYNC_OPTIONS+=" --progress --stats --verbose -h "
+[[ ${VERBOSE} -ne 0 ]] && RSYNC_OPTIONS+=" --progress --stats --verbose --human-readable "
 
 userAndHost="${POSTDUMP_COPY_BORG_REPOSITORY_RSYNC_SSH_USER}@${POSTDUMP_COPY_BORG_REPOSITORY_RSYNC_SSH_HOST}"
 outputDirectory="${POSTDUMP_COPY_BORG_REPOSITORY_RSYNC_SSH_PATH}/${HOSTNAME}"
 
-ssh -p ${POSTDUMP_COPY_BORG_REPOSITORY_RSYNC_SSH_PORT} ${userAndHost} "mkdir -p ${outputDirectory}"
+TEMP_DIR=`mktemp -d`
+sshfs -p ${POSTDUMP_COPY_BORG_REPOSITORY_RSYNC_SSH_PORT} ${userAndHost}: ${TEMP_DIR}
+mkdir -p ${TEMP_DIR}/${outputDirectory}
+umount ${TEMP_DIR}
+rmdir ${TEMP_DIR}
 
 rsync ${RSYNC_OPTIONS} -e "ssh -p ${POSTDUMP_COPY_BORG_REPOSITORY_RSYNC_SSH_PORT}" ${BORG_REPOSITORY_PATH} ${userAndHost}:${outputDirectory}
 
